06acf9812d9d99b0069ae29d51a4da3bb8b2265c
Bastian Inuk Christensen <jonalist@gmail.com>
2019-03-24 11:09:58 +0100

    Hot SwiftPM Fix

--- Package.swift
+++ Package.swift
@@ -20,7 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ObjectivePGP",
-            dependencies: []),
+            dependencies: [],
+            path: "."
+        )
         .testTarget(
             name: "ObjectivePGPTests",
             dependencies: ["ObjectivePGP"]),
